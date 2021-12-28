3f8b63cb8438f8c12074aa2dca4b3a3fe77c0430
Hackmaster3120 <81357992+Hackmaster3120@users.noreply.github.com>
2021-12-28 15:50:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-noseX=0;
-noseY=0;
+RwX=0;
+LwX=0;
+size=0;
 function setup() {
     canvas=createCanvas(600,400)
+    video = createCapture(VIDEO);
     canvas.position(670,130);
+    video.size(600,400);
+    poseNAT=ml5.poseNet(video,modelLoaded);
+    poseNAT.on('pose',gotResults);
 }
 function draw() {
-    rect(noseX,noseY,10,10)
+    background("white")
+    textSize(size)
+    fill("lightgreen")
+    text("Notice",100,100)
+}
+function modelLoaded() {
+    console.log("dots on your face have been loaded");
 }
 function gotResults(results) {
     if(results.length>0){
         console.log(results)
-        noseX=results[0].pose.nose.x;
-        noseY=results[0].pose.nose.y;
+        RwX=results[0].pose.rightWrist.x;
+        LwX=results[0].pose.leftWrist.x;
+        size=(Math.ceil(RwX-LwX))+400;
+        document.getElementById("WHspan").innerHTML=size+"px";
     }
 }
